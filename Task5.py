857c45314d777ca43219618d1d0e2dbda29b0f7c
IliaV <iliaspb2012@gmail.com>
2022-10-01 22:29:38 +0300

    task5

---
+++ Task5.py
@@ -0,0 +1,10 @@
+# Задание 5 Реализуйте алгоритм перемешивания списка.
+
+from random import random
+
+
+lst_a = list[1,2,3,4,5]
+lst_b = []
+lst_b == random.randint(lst_a)
+print(lst_a)
+print(lst_b)
