69e37a7ff527eca416a7e351bf0db8a552302052
IliaV <iliaspb2012@gmail.com>
2022-10-04 17:47:54 +0300

    task5_corection

--- Task5.py
+++ Task5.py
@@ -1,10 +1,26 @@
 # Задание 5 Реализуйте алгоритм перемешивания списка.
 
-from random import random
+
+import random
 
 
-lst_a = list[1,2,3,4,5]
-lst_b = []
-lst_b == random.randint(lst_a)
-print(lst_a)
-print(lst_b)
+def mix_lst(lst_original):
+    
+    
+    lst = lst_original[:]
+    lst_length = len(lst)
+    for i in range(lst_length):
+        # Получение случайного индекса
+        index = random.randint(0, lst_length - 1)
+        # Замена
+        temp = lst[i]
+        lst[i] = lst[index]
+        lst[index] = temp
+    # Возвращаем список
+    return lst
+lst = [1, 2, 3, 4, 5, 6, 7]
+mixed_lst = mix_lst(lst)
+print(lst)
+print(mixed_lst)
+
+
